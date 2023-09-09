8ea257674e7fe1ed7d14669fd21437bfaf259c26
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-09-09 21:56:24 +0200

    .

---
+++ Septembar 2023/2.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Struktura za stek
+struct Stack {
+    int capacity;
+    int top;
+    int* array;
+};
+
+// Funkcija za inicijalizaciju steka
+struct Stack* createStack(int capacity) {
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = (int*)malloc(stack->capacity * sizeof(int));
+    return stack;
+}
+
+// Funkcija za proveru da li je stek prazan
+int isEmpty(struct Stack* stack) {
+    return (stack->top == -1);
+}
+
+// Funkcija za proveru da li je stek pun
+int isFull(struct Stack* stack) {
+    return (stack->top == stack->capacity - 1);
+}
+
+// Funkcija za dodavanje elementa u stek
+void push(struct Stack* stack, int item) {
+    if (isFull(stack)) {
+        printf("Stek je pun. Element %d nije dodat.\n", item);
+        return;
+    }
+    stack->array[++stack->top] = item;
+}
+
+// Funkcija za uklanjanje elementa iz steka
+int pop(struct Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Stek je prazan.\n");
+        return -1;
+    }
+    return stack->array[stack->top--];
+}
+
+// Funkcija za dodavanje elementa u red
+void enqueue(struct Stack* stack1, struct Stack* stack2, int item) {
+    // Premestiti sve elemente iz stack1 u stack2
+    while (!isEmpty(stack1)) {
+        push(stack2, pop(stack1));
+    }
+    
+    // Dodati novi element u stack1
+    push(stack1, item);
+    
+    // Premestiti sve elemente iz stack2 nazad u stack1
+    while (!isEmpty(stack2)) {
+        push(stack1, pop(stack2));
+    }
+}
+
+// Funkcija za uklanjanje elementa iz reda
+int dequeue(struct Stack* stack1) {
+    if (isEmpty(stack1)) {
+        printf("Red je prazan.\n");
+        return -1;
+    }
+    return pop(stack1);
+}
+
+int main() {
+    struct Stack* stack1 = createStack(100); // Prvi stek za red
+    struct Stack* stack2 = createStack(100); // Drugi stek za privremeno skladištenje
+
+    enqueue(stack1, stack2, 1);
+    enqueue(stack1, stack2, 2);
+    enqueue(stack1, stack2, 3);
+
+    printf("Element iz reda: %d\n", dequeue(stack1));
+    printf("Element iz reda: %d\n", dequeue(stack1));
+
+    enqueue(stack1, stack2, 4);
+    
+    printf("Element iz reda: %d\n", dequeue(stack1));
+    printf("Element iz reda: %d\n", dequeue(stack1));
+
+    return 0;
+}
---
+++ Septembar 2023/3.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Struktura za čvor binarnog stabla
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Funkcija za kreiranje novog čvora
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Funkcija za ispisivanje elemenata sa određenog nivoa stabla
+void printLevel(struct Node* root, int level) {
+    if (root == NULL)
+        return;
+    if (level == 1)
+        printf("%d ", root->data);
+    else if (level > 1) {
+        printLevel(root->left, level - 1);
+        printLevel(root->right, level - 1);
+    }
+}
+
+// Funkcija za ispisivanje elemenata sa određenog nivoa stabla
+void printLevelOrder(struct Node* root, int level) {
+    if (root == NULL)
+        return;
+    
+    for (int i = 1; i <= level; i++) {
+        printf("Nivo %d: ", i);
+        printLevel(root, i);
+        printf("\n");
+    }
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+
+    printf("Ispisivanje elemenata sa određenog nivoa stabla:\n");
+    int level = 2; // Nivo koji želimo ispisati
+    printLevelOrder(root, level);
+
+    return 0;
+}
