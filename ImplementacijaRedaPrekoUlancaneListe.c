009b7c88be52e235437b83d8a2e54dfcfe863404
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-09-17 00:20:42 +0200

    .

--- ImplementacijaRedaPrekoUlancaneListe.c
+++ ImplementacijaRedaPrekoUlancaneListe.c
@@ -10,3 +10,61 @@ typedef struct node{
     void Display();
     NODE *front,*rear;
 
+// Operacija Enqueue (dodavanje elementa)
+
+void Insert(int info){
+    NODE *temp;
+    temp=(NODE *)malloc(sizeof(NODE));
+    if( temp == NULL){
+        printf(" Out of Memory !!Overflow !!!");
+    }
+    else{
+        temp->data=info;
+        temp->link=NULL;
+        if(front == NULL) { 
+            front = rear =temp; 
+        } /* First Node? */
+        else{ 
+            rear->link=temp; rear = temp;
+        } /* Insert End */
+            printf(" Node has beeninserted at End Successfully !!");
+    }
+}
+
+// Operacija Dequeue (uklanjanje elementa)
+
+int Delete()
+{
+    int info;
+    NODE *t;
+    if( front ==  NULL) { printf(" Underflow!!!"); return -1; }
+    else
+    {
+        t=front;
+        info=front->data;
+        if(front == rear) rear=NULL;
+        front=front->link;
+        t->link=NULL;
+        free(t);
+        return(info);
+    }
+}
+
+// Prikaz (štampanje) elemenata
+
+void Display()
+{
+    NODE *t;
+    if( front == NULL) printf("Empty Queue\n");
+    else
+    {
+        t=front;
+        printf("Front->");
+        while(t)
+        {
+            printf("[%d]->",t->data);
+            t=t->link;
+        }
+        printf("Rear\n");
+    }
+}
