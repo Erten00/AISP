c20a1cf688079d33a960f107685be540bd9ae378
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-09-16 23:09:06 +0200

    .

--- Pseudocode.c
+++ Pseudocode.c
@@ -264,4 +264,53 @@ void ReversePrint() {
     temp = temp->prev;
     }
     printf("\n");
+
+}
+
+// Kružno povezana lista
+
+// Struktura elementa
+
+typedef struct NODE{
+    int info;
+    struct NODE *next;
+} NODE;
+NODE *front=NULL,*rear=NULL,*temp;
+
+// Kreiranje elementa
+
+void create(){ 
+    NODE *newnode;
+    newnode=(node*)malloc(sizeof(node));
+    printf("\nEnter the node value : ");
+    scanf("%d",&newnode->info); newnode->next=NULL;
+    if(rear==NULL){
+        front=rear=newnode;
+    }
+    else { 
+        rear->next=newnode; rear=newnode;
+    }
+    rear->next=front;
+}
+
+// Brisanje elemenata
+
+void del(){ 
+    temp=front;
+    if(front==NULL){
+        printf("\nUnderflow :");
+    }
+    else {
+        if(front==rear){
+            printf("\n%d",front->info);
+            front=rear=NULL; 
+        }
+        else{
+            printf("\n%d",front->info);
+            front=front->next;
+         rear->next=front;
+        }
+        temp->next=NULL;
+        free(temp);
+    }
 }
